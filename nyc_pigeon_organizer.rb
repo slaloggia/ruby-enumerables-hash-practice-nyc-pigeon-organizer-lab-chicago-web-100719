5aeac2d93a1afe200672455e50a917ec7b4df09b
Sarah Laloggia <sarah.laloggia@gmail.com>
2019-09-26 20:47:37 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,9 +1,9 @@
 def nyc_pigeon_organizer(data)
   pigeon_hash = {}
-  pigeon_hash = data.each{|attribute_hash| 
+  data.each{|attribute_hash| 
   attribute_hash.each do 
     |attribute| attribute.reduce({}) do
-      |memo, (key, value)| pigeon_hash[]
+      |memo, (key, value)| memo[]
     end
   end
     
